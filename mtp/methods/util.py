9f56aa55b56d5eb6883af27afef6b09d0a16dac4
Steven Gans <stevenbgans@gmail.com>
2025-05-25 17:15:53 -0400

    Delete mtp directory

--- mtp/methods/util.py
+++
@@ -1,9 +0,0 @@
-def get_possible_emojis():
-    emojis = []
-    for code_point in range(0x1F601, 0x1F64F):
-        try:
-            emoji = chr(code_point)
-            emojis.append(emoji)
-        except ValueError:
-            continue
-    return emojis
