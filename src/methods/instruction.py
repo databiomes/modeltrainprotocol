44a2c7deef07c5d56c7f8cf81683f81aae44a944
Nikita Olechko <nikita.olechko@gmail.com>
2025-08-29 14:03:56 -0700

    Add initial documentation to Instruction

--- src/methods/instruction.py
+++ src/methods/instruction.py
@@ -1,12 +1,25 @@
-class Instruction:
-    def __init__(self, tokens, result, memory):
-        self.tokens = tokens
-        self.result = result
-        self.memory = memory
-        self.samples = []
-        self.ppo = []
+from src.methods.token import Token
 
-    def add_sample(self, strings, prompt=None, numbers=None, value=None):
+
+class Instruction:
+    """
+    Class representing an instruction with tokens, result, memory, and samples.
+
+    An Instruction is a set of tokens that show possible input combinations for a model.
+
+    Samples must be added to the Instruction to provide context for the model.
+    A minimum of 3 samples must be added to an Instruction.
+    """
+
+    def __init__(self, tokens: list[tuple[Token]], result: Token, memory: int):
+        """Initializes an Instruction instance."""
+        self.tokens: list[tuple[Token]] = tokens
+        self.result: Token = result
+        self.memory: int = memory
+        self.samples: list[dict] = []
+
+    def add_sample(self, strings: list[str], prompt=None, numbers=None, value=None):
+        """Add a sample to the Instruction."""
         assert len(strings) >= self.memory, "The number of lines does not match the memory size."
         if numbers is not None:
             self.number_check(numbers)
@@ -16,19 +29,6 @@ class Instruction:
             value = "None"
         self.samples.append({'strings': strings, 'prompt': prompt, 'number': numbers, 'result': self.result,
                              'value': value})
-
-    def add_ppo_sample(self, strings, prompt=None, numbers=None, value=None, a_output="", b_output="", pref=""):
-        assert type(strings) == list, "Strings is not a list."
-        assert pref == "A" or pref == "B", "Preference must be A or B."
-        assert len(strings) >= self.memory - 1, "The number of lines does not match the memory size minus 1."
-        if numbers is not None:
-            self.number_check(numbers)
-        if value is not None:
-            assert int(value) or float(value), "Value is not a number."
-        else:
-            value = "None"
-        self.ppo.append({'strings': strings, 'prompt': prompt, 'number': numbers, 'result': self.result,
-                         'value': value, 'a_sample': a_output, 'b_sample': b_output, 'pref': pref})
 
     def number_check(self, numbers):
         assert isinstance(numbers, list), "The number is not set as a list."
@@ -44,11 +44,13 @@ class Instruction:
             assert len(num) == len(num_token), "Not all numbers provided."
 
     def __str__(self):
+        """String representation of the Instruction."""
         tokens_str = ', '.join([''.join([token.key for token in token_tuple]) for token_tuple in self.tokens])
         samples_str = ',\n'.join([f"Sample(Strings: {sample['strings']}, Result: {sample['result'].key + sample['value'] if sample['value'] is not None else ''}" for sample in self.samples])
         return f"Token Set(Tokens: {tokens_str}, Result: {self.result.key}, Samples:\n{samples_str})"
 
     def to_dict(self):
+        """Convert the Instruction to a dictionary representation."""
         return {
             'tokens': [[token.to_dict() for token in token_tuple] for token_tuple in self.tokens],
             'result': self.result.to_dict() if self.result else None,
