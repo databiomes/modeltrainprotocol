8a4ef26a0c75426010cab80d1b0d08127b92f395
Nikita Olechko <nikita.olechko@gmail.com>
2025-08-28 22:56:49 -0700

    Update get_possible_emojis to return a set

--- src/methods/util.py
+++ src/methods/util.py
@@ -1,10 +1,10 @@
-def get_possible_emojis() -> list[str]:
+def get_possible_emojis() -> set[str]:
     """Generate a list of possible emojis from Unicode code points."""
-    emojis: list[str] = []
+    emojis: set[str] = set()
     for code_point in range(0x1F601, 0x1F64F):
         try:
             emoji: str = chr(code_point)
-            emojis.append(emoji)
+            emojis.add(emoji)
         except ValueError:
             continue
     return emojis
