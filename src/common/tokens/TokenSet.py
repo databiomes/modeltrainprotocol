50cf5a261706c6e566f70c315a6d11bff6c3de8f
Nikita Olechko <nikita.olechko@gmail.com>
2025-09-02 16:25:08 -0700

    Introduce Snippet class to represent part of an Instruction Sample

--- src/common/tokens/TokenSet.py
+++ src/common/tokens/TokenSet.py
@@ -1,6 +1,15 @@
+import dataclasses
 from typing import Sequence
 
+from dataclasses import dataclass
+
 from src.common.tokens.Token import Token
+
+
+@dataclass
+class Snippet:
+    sample: str
+    numbers: list[int] = dataclasses.field(default_factory=list)
 
 
 class TokenSet:
@@ -10,8 +19,15 @@ class TokenSet:
         """Initializes a TokenSet instance."""
         self.tokens: Sequence[Token] = tokens
         self.is_user: bool = any(token.user for token in tokens)
-        self.requires_number: bool = any(token.num for token in tokens)
+        self.required_numbers: int = sum(1 for token in tokens if token.num)  # Count of tokens that require numbers
         self.key: str = ''.join(token.key for token in tokens)
+
+    def create_snippet(self, sample: str, numbers: list[int] = None) -> Snippet:
+        """Create a snippet for the TokenSet"""
+        numbers = numbers if numbers is not None else []
+        assert len(numbers) == self.required_numbers, \
+            f"{self} requires {self.required_numbers} numbers but {len(numbers)} were provided."
+        return Snippet(sample=sample, numbers=numbers)
 
     def __repr__(self):
         """String representation of the TokenSet."""
