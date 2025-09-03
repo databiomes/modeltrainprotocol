e110d156f61227315d612c7da830ad059b204f12
DESKTOP-BSTBD3N\Nikita <nikita.olechko@gmail.com>
2025-09-03 15:50:19 -0700

    Update Guardrail to be set on TokenSets, not instructions

--- src/common/Guardrail.py
+++ src/common/Guardrail.py
@@ -1,13 +1,13 @@
-from src.common.instructions.UserInstruction import UserInstruction
+from src.common.tokens.TokenSet import TokenSet
 
 
 class Guardrail:
-    """Defines a guardrail response to bad prompts. Guardrails are set on a UserInstruction."""
+    """Defines a guardrail response to bad prompts. Guardrails are set on a TokenSet."""
 
-    def __init__(self, instruction: UserInstruction, good_prompt: str, bad_prompt: str, bad_output: str):
+    def __init__(self, token_set: TokenSet, good_prompt: str, bad_prompt: str, bad_output: str):
         """
         Initializes a Guardrail.
-        :param instruction: The Instruction the guardrail is set on.
+        :param token_set: The TokenSet the guardrail is set on.
         :param good_prompt: Description of a good prompt.
         :param bad_prompt: Description of a bad prompt.
         :param bad_output: The output the model should produce when a bad prompt is detected.
@@ -17,12 +17,15 @@ class Guardrail:
             bad_prompt="Quote being spoken that is irrelevant and off-topic with 1-20 words",
             output="I have no idea what you're talking about."
         """
-        self.instruction: UserInstruction = instruction
+        if not isinstance(token_set, TokenSet):
+            raise TypeError("Guardrails can only be set on a TokenSet.")
+
+        self.token_set: TokenSet = token_set
         self.good_prompt: str = good_prompt
         self.bad_prompt: str = bad_prompt
         self.bad_output: str = bad_output
         self.samples: list[str] = []
-        self.key: str = ""
+        self.key: str = self.token_set.key
 
     def add_sample(self, sample: str):
         """
