4bc98d55526606e8c4550147cb824fba57ac01c4
Nikita Olechko <nikita.olechko@gmail.com>
2025-09-04 19:07:37 -0700

    Create specialized token sets to prevent users from issuing mutually exclusive token attributes

--- src/common/tokens/Token.py
+++ src/common/tokens/Token.py
@@ -1,23 +1,20 @@
 class Token:
-    def __init__(self, value: str, key: str, desc: str | None = None, special: str | None = None,
-                 user: bool = False, num: bool = False, default: bool = False):
+    """The lowest level unit for a model. Represents a word, symbol, or concept."""
+
+    def __init__(self, value: str, key: str, desc: str | None = None):
         """
         Initializes a Token instance.
 
         :param value: The string representing the token's value.
         :param key: The key associated with the token, a symbol, emoji, or short string.
         :param desc: Optional description of the token. Extends the value to contextualize its use.
-        :param special: Optional special attribute to identify special tokens.
-        :param user: Boolean indicating if this token represents a user input.
-        :param num: Boolean indicating if this token is associated with a numerical value.
-        :param default: Boolean indicating if this is a default token (like <BOS>, <EOS>).
         """
-        self.value: str = value + "_" if not default else value
+        self.value: str = value + "_"
         self.key: str = key
-        self.user: bool = user
-        self.num: bool = num
         self.desc: str = desc
-        self.special: str = special
+        self.user: bool = False
+        self.num: bool = False
+        self.special: str | None = None
 
     def __str__(self):
         """String representation of the token."""
@@ -30,9 +27,9 @@ class Token:
     def __eq__(self, other):
         """
         Defines equality based on the string.
-        Returns True if the other object is a Token and its string matches this token's string.
+        Returns True if the other object is of the same Token subclass and its string matches this token's string.
         """
-        return isinstance(other, Token) and self.value == other.value
+        return isinstance(other, self.__class__) and self.value == other.value
 
     def __dict__(self):
         """Dictionary representation of the token."""
