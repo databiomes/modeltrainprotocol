004edc1839e51b971a5ab19ff30087aaa21f15b5
Nikita Olechko <nikita.olechko@gmail.com>
2025-09-10 15:33:28 -0700

    Add NumListToken to tokens __init__

--- model_train_protocol/common/tokens/__init__.py
+++ model_train_protocol/common/tokens/__init__.py
@@ -5,6 +5,7 @@ Token classes for the Model Train Protocol package.
 from .Token import Token
 from .UserToken import UserToken
 from .NumToken import NumToken
+from .NumListToken import NumListToken
 from .SpecialToken import SpecialToken
 from .DefaultSpecialToken import DefaultSpecialToken
 from .TokenSet import TokenSet, Snippet
@@ -13,6 +14,7 @@ __all__ = [
     "Token",
     "UserToken", 
     "NumToken",
+    "NumListToken",
     "SpecialToken",
     "DefaultSpecialToken",
     "TokenSet",
