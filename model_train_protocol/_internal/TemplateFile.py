2e038777e9494f072d7a0e36de1e5b8e65ab4a3a
Nikita Olechko <nikita.olechko@gmail.com>
2025-09-17 18:09:09 -0700

    Add examples to Template

--- model_train_protocol/_internal/TemplateFile.py
+++ model_train_protocol/_internal/TemplateFile.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Collection
 
-from model_train_protocol import NumToken
+from model_train_protocol import NumToken, SimpleInstruction, UserInstruction
 from model_train_protocol.common.constants import BOS_TOKEN, RUN_TOKEN, EOS_TOKEN
 from model_train_protocol.common.instructions import Instruction
 
@@ -13,12 +13,8 @@ class TemplateFile:
     class ExampleUsage:
         """Stores example usages of the template."""
 
-        valid_simple_input: str
-        valid_user_input: str
-        valid_output: str
-
-        guardrail_input: str
-        guardrail_output: str
+        input: str
+        output: str
 
     class ModelInput:
         """Represents inputs to the model."""
@@ -34,11 +30,12 @@ class TemplateFile:
                     token_strings = "".join([t.value for t in token_set])
                     token_keys = []
                     for token in token_set:
-                        token_keys.append(token.key + (token.protocol_representation if isinstance(token, NumToken) else ""))
+                        token_keys.append(
+                            token.key + (token.protocol_representation if isinstance(token, NumToken) else ""))
                     token_keys = "".join(token_keys)
                     unique_sets[idx].add(str(token_strings) + ": " + (
                         (str(token_keys) + "USER PROMPT") if any(token_user) and (
-                                    idx == (len(unique_sets) - 1)) else str(
+                                idx == (len(unique_sets) - 1)) else str(
                             token_keys)) + "\n" + ("<string>" if idx != (len(instruction.context) - 1) else ""))
 
             for input_set in unique_sets.values():
@@ -95,11 +92,57 @@ class TemplateFile:
         self.model_input.add_inputs_from_instructions(self.instructions, context_lines=self.context_lines)
         self.model_output.add_results_from_instructions(self.instructions)
 
+    def _create_sample_model_output(self):
+        """Creates a sample model output string for example usages."""
+        sample_output: str = ""
+        sample_output += self.model_output.model_response + "\n"
+        first_output_key = next(iter(self.model_output.model_results))
+        sample_output += self.model_output.model_results[first_output_key] + "\n"
+        sample_output += EOS_TOKEN.key
+        return sample_output
+
+    def _create_examples(self) -> dict[str, str]:
+        """
+        Creates example usages of the template.
+
+        Creates a simple instruction example and a user instruction example if available.
+        """
+        examples: dict[str, str] = dict()
+        simple_instruction: SimpleInstruction = next(
+            (i for i in self.instructions if isinstance(i, SimpleInstruction)), None)
+        user_instruction: UserInstruction = next(
+            (i for i in self.instructions if isinstance(i, UserInstruction)), None)
+
+        if simple_instruction:
+            simple_input: str = ""
+            for token_set in simple_instruction.get_token_sets():
+                token_strings = "".join([token.key for token in token_set])
+                simple_input += token_strings + "\n"
+                simple_input += "<string>\n"
+            simple_input = BOS_TOKEN.key + "\n" + simple_input + RUN_TOKEN.key + "\n"
+            examples["simple_instruction_input"] = simple_input + self._create_sample_model_output()
+
+        if user_instruction:
+            user_input: str = ""
+            for idx, token_set in enumerate(user_instruction.get_token_sets()):
+                token_strings = "".join([token.key for token in token_set])
+                user_input += token_strings + "\n"
+                user_input += "<string>\n" if idx != (len(user_instruction.get_token_sets()) - 1) else "USER PROMPT\n"
+            user_input = BOS_TOKEN.key + "\n" + user_input + RUN_TOKEN.key + "\n"
+            examples["user_instruction_input"] = user_input
+
+        examples["output"] = self._create_sample_model_output()
+
+        return examples
+
     def to_json(self) -> dict:
         """Converts the entire template to a JSON-serializable dictionary."""
-        return {
+        examples: dict[str, str] = self._create_examples()
+        json_dict: dict = {
             "all_combinations": {
                 "model_input": self.model_input.to_json(),
                 "model_output": self.model_output.to_json()
-            }
+            },
+            "example_usage": examples
         }
+        return json_dict
