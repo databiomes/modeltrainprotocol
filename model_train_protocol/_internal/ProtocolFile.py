2690018b0fcb2eb845158544a90ff57268841aa9
Nikita Olechko <nikita.olechko@gmail.com>
2025-09-29 09:51:02 -0700

    Update ProtocolFile.py to use pydantic types for validation

--- model_train_protocol/_internal/ProtocolFile.py
+++ model_train_protocol/_internal/ProtocolFile.py
@@ -4,7 +4,8 @@ from typing import Collection
 from model_train_protocol import Token, NumToken
 from model_train_protocol.common.constants import UNK_TOKEN
 from model_train_protocol.common.guardrails import Guardrail
-from model_train_protocol.common.instructions import Instruction
+from model_train_protocol.common.pydantic.protocol import Instruction, TokenInfo, Sample, InstructionSet, Numbers, \
+    Batches, ProtocolModel
 from model_train_protocol.common.tokens import TokenSet, SpecialToken
 
 
@@ -146,6 +147,11 @@ class ProtocolFile:
                 if 'strings' in sample:
                     sample['sample'] = sample.pop('strings')
 
+            # Rename null values to "None"
+            for sample in instruction['samples']:
+                if sample['value'] is None:
+                    sample['value'] = "None"
+
         return protocol_json
 
     def _get_special_token_keys(self):
@@ -157,24 +163,73 @@ class ProtocolFile:
         return sorted(self._special_token_keys | self._instruction_token_keys)
 
     def to_json(self):
-        """Converts the template to a JSON-compatible dictionary."""
-        json_dict = {
-            "name": self._name,
-            "context": self._context,
-            "tokens": dict(sorted(self._tokens.items())),
-            "special_tokens": self._get_special_token_keys(),
-            "instruction": {
-                "memory": self._instruction.context_lines + 1, # +1 for the response line
-                "sets": [vars(s) for s in self._instruction.sets],
-            },
-            "guardrails": self._guardrails,
-            "numbers": self._numbers,
-            "batches": {
-                "pretrain": self._batches.pretrain,
-                "instruct": self._batches.instruct,
-                "judge": self._batches.judge,
-                "ppo": self._batches.ppo,
-            },
-        }
+        """Converts the template to a JSON-compatible dictionary using Pydantic models."""
 
+        # Create TokenInfo objects for each token
+        token_info_dict = {}
+        for token_value, token_dict in self._tokens.items():
+            token_info = TokenInfo(
+                emoji=token_dict.get('emoji', ''),
+                num=token_dict.get('num', False),
+                user=token_dict.get('user', False),
+                desc=token_dict.get('desc'),
+                special=token_dict.get('special')
+            )
+            token_info_dict[token_value] = token_info
+
+        # Create InstructionSet objects
+        instruction_sets = []
+        for instruction_set in self._instruction.sets:
+            # Create Sample objects
+            samples = []
+            for sample_data in instruction_set.samples:
+                sample = Sample(
+                    sample=sample_data.get('strings', []),
+                    prompt=sample_data.get('prompt', ''),
+                    number=sample_data.get('number', []),
+                    result=sample_data.get('result', ''),
+                    value=sample_data.get('value', '')
+                )
+                samples.append(sample)
+
+            # Create InstructionSet
+            instruction_set_obj = InstructionSet(
+                set=instruction_set.set,
+                result=instruction_set.result,
+                samples=samples,
+                ppo=instruction_set.ppo
+            )
+            instruction_sets.append(instruction_set_obj)
+
+        # Create Instruction object
+        instruction = Instruction(
+            memory=self._instruction.context_lines + 1,  # +1 for the response line
+            sets=instruction_sets
+        )
+
+        # Create Numbers object
+        numbers = Numbers()
+
+        # Create Batches object
+        batches = Batches(
+            pretrain=self._batches.pretrain,
+            instruct=self._batches.instruct,
+            judge=self._batches.judge,
+            ppo=self._batches.ppo
+        )
+
+        # Create ProtocolModel
+        protocol = ProtocolModel(
+            name=self._name,
+            context=self._context,
+            tokens=token_info_dict,
+            special_tokens=self._get_special_token_keys(),
+            instruction=instruction,
+            guardrails=self._guardrails,
+            numbers=numbers,
+            batches=batches
+        )
+
+        # Convert to JSON and apply backwards compatibility transformations
+        json_dict = protocol.model_dump(by_alias=True)
         return self._rename_protocol_elements(json_dict)
