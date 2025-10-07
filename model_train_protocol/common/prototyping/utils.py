88b3e26c599d8cfd5d693f0eb27243101b14d485
Nikita Olechko <nikita.olechko@gmail.com>
2025-10-07 19:46:30 -0400

    Add attributes to final_token

--- model_train_protocol/common/prototyping/utils.py
+++ model_train_protocol/common/prototyping/utils.py
@@ -2,25 +2,26 @@ from model_train_protocol import NumToken, NumListToken, Token, TokenSet
 from model_train_protocol.common.pydantic.protocol import TokenInfoModel
 
 
-def add_token_attributes(prototype_model_json: dict,
-                         token_subsets=None) -> dict:
+def add_token_attributes(prototype_model_json: dict) -> dict:
     """
     Adds 'value' and 'special' attributes to each token in the specified subsets of tokens within the protocol JSON.
 
     :param prototype_model_json: The protocol JSON dictionary containing instruction sets and tokens.
-    :param token_subsets: A list of token subset names to process (default is ["prompt_tokens", "response_tokens"]).
     :return: The modified protocol JSON dictionary with added attributes.
     """
-    if token_subsets is None:
-        token_subsets = ["prompt_tokens", "response_tokens"]
-
     for i, instruction in enumerate(prototype_model_json["instruction_sets"]):
-        for token_subset in token_subsets:
+        for token_subset in ["prompt_tokens", "response_tokens"]:
             for j, token in enumerate(instruction[token_subset]):
                 prototype_model_json['instruction_sets'][i][token_subset][j]["value"] = \
                     prototype_model_json['instruction_sets'][i][token_subset][j]["key"]
                 prototype_model_json['instruction_sets'][i][token_subset][j]["special"] = None
                 prototype_model_json['instruction_sets'][i][token_subset][j]["user"] = False
+
+    # Add attributes to final_token
+    prototype_model_json['final_token']["value"] = \
+        prototype_model_json['final_token']["key"]
+    prototype_model_json['final_token']["special"] = None
+    prototype_model_json['final_token']["user"] = False
 
     return prototype_model_json
 
