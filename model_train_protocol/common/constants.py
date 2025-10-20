c6d2c067264d8fdab842b1b97ef9502b928488c5
Nikita Olechko <nikita.olechko@gmail.com>
2025-10-20 14:56:48 -0300

    Remove hardcoded emojis from special token constants

--- model_train_protocol/common/constants.py
+++ model_train_protocol/common/constants.py
@@ -6,16 +6,3 @@ EOS_TOKEN: SpecialToken = SpecialToken(value="<EOS>", key="<EOS>", special="end"
 RUN_TOKEN: SpecialToken = SpecialToken(value="<RUN>", key="<RUN>", special="infer")
 PAD_TOKEN: SpecialToken = SpecialToken(value="<PAD>", key="<PAD>", special="pad")
 UNK_TOKEN: SpecialToken = SpecialToken(value="<UNK>", key="<UNK>", special="unknown")
-
-# TODO: Remove this code when emoji dependency is removed
-"""Assign default emoji keys to special tokens for backward compatibility."""
-special_token_emoji_map = {
-    NON_TOKEN.value: "🫙",
-    BOS_TOKEN.value: "🏁",
-    EOS_TOKEN.value: "🎬",
-    RUN_TOKEN.value: "🏃",
-    PAD_TOKEN.value: "🗒",
-    UNK_TOKEN.value: "🛑"
-}
-for token in [NON_TOKEN, BOS_TOKEN, EOS_TOKEN, RUN_TOKEN, PAD_TOKEN, UNK_TOKEN]:
-    token.key = special_token_emoji_map[token.value]
