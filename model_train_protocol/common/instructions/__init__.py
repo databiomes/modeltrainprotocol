cd5bc7f5a64ba1132939006f8903cb1bf4cadf92
Nikita Olechko <nikita.olechko@gmail.com>
2025-11-26 12:54:55 -0800

    Update import order

--- model_train_protocol/common/instructions/__init__.py
+++ model_train_protocol/common/instructions/__init__.py
@@ -3,10 +3,10 @@ Instruction classes for the Model Train Protocol package.
 """
 
 from .BaseInstruction import BaseInstruction
-from .Instruction import Instruction
-from .ExtendedInstruction import ExtendedInstruction
 from .helpers.Response import Response
 from .helpers.ExtendedResponse import ExtendedResponse
+from .Instruction import Instruction
+from .ExtendedInstruction import ExtendedInstruction
 
 __all__ = [
     "BaseInstruction",
--- model_train_protocol/common/tokens/__init__.py
+++ model_train_protocol/common/tokens/__init__.py
@@ -4,6 +4,7 @@ Token classes for the Model Train Protocol package.
 
 from .Token import Token
 from .FinalToken import FinalToken
+from .FinalNumToken import FinalNumToken
 from .NumToken import NumToken
 from .NumListToken import NumListToken
 from .SpecialToken import SpecialToken
@@ -13,6 +14,7 @@ from .TokenSet import TokenSet, Snippet
 __all__ = [
     "Token",
     "FinalToken",
+    "FinalNumToken",
     "NumToken",
     "NumListToken",
     "SpecialToken",
