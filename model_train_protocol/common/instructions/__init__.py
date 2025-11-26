19f45054181067e43f769530412d5a74cd44c34c
Nikita Olechko <nikita.olechko@gmail.com>
2025-11-25 16:25:56 -0800

    Update exports to include ExtendedResponse

--- model_train_protocol/common/instructions/__init__.py
+++ model_train_protocol/common/instructions/__init__.py
@@ -6,10 +6,12 @@ from .BaseInstruction import BaseInstruction
 from .Instruction import Instruction
 from .ExtendedInstruction import ExtendedInstruction
 from .helpers.Response import Response
+from .helpers.ExtendedResponse import ExtendedResponse
 
 __all__ = [
     "BaseInstruction",
     "Instruction",
     "ExtendedInstruction",
     "Response",
+    "ExtendedResponse",
 ]
