c5d5a2f57bc6a76d47e812956d0c7752952eac27
Nikita Olechko <nikita.olechko@gmail.com>
2026-02-10 10:40:09 -0800

    Add get_template_schema_url() to utils

--- model_train_protocol/utils.py
+++ model_train_protocol/utils.py
@@ -23,11 +23,18 @@ def get_version():
     return str(pyproject["project"]["version"])
 
 
-def get_schema_url():
+def get_bloom_schema_url():
     """
     Retrieves the schema URL for the current version of the Model Train Protocol.
     """
-    # Add conventional schema tag to JSON for validation purposes
     version_semantic: str = get_version()
     schema_url = f"https://mtp.schemas.databiomes.com/v{version_semantic[0]}/bloom_{version_semantic.replace('.', '_')}.json"
+    return schema_url
+
+def get_template_schema_url():
+    """
+    Retrieves the schema URL for the current version of the MTP Template.
+    """
+    version_semantic: str = get_version()
+    schema_url = f"https://mtp.schemas.databiomes.com/v{version_semantic[0]}/template_{version_semantic.replace('.', '_')}.json"
     return schema_url
