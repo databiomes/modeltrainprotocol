4bab145256dd3a560887e6cd2ebc81fc2e45df73
Nikita Olechko <nikita.olechko@gmail.com>
2026-02-12 14:42:56 -0800

    Remove ID

--- model_train_protocol/_internal/csv/conversion.py
+++ model_train_protocol/_internal/csv/conversion.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 import pandas as pd
 
@@ -9,11 +10,10 @@ from model_train_protocol.common.constants import NON_TOKEN
 @dataclass
 class CSVLine:
     """This class represents a single line in the CSV data."""
-    id: str
     input_str: str
     output_str: str
     context_str: str
-    requires_str: str
+    required_str: str
 
     @property
     def is_guardrail(self) -> bool:
@@ -24,11 +24,10 @@ class CSVLine:
 class CSVConversion:
     """This class provides methods to convert from CSV into MTP"""
     instruction_name: str = "Output"
-    id_col: str = "ID"
     input_col: str = "Input"
     output_col: str = "Output"
+    required_col: str = "Required"
     context_col: str = "Output Reference"
-    required_col: str = "Requires"
 
     input_token: Token = Token("Input")
     input_tokenset: TokenSet = TokenSet(tokens=[input_token])
@@ -39,31 +38,41 @@ class CSVConversion:
 
         :param csv_data: A dictionary where keys are column names and values are lists of column data.
         """
-        self.csv_data = self._process_dataframe(csv_data)
-        self.line_by_id: dict[str, CSVLine] = self._identify_lines()
+        self.csv_data: pd.DataFrame = self._process_dataframe(csv_data)
+        self.ordered_lines: List[CSVLine] = self._format_lines()
         self.protocol: Protocol = Protocol(name="CSV Protocol", inputs=2, encrypt=False)
         self.required_token: Token = self._generate_required_token()
         self.required_tokenset: TokenSet = TokenSet(tokens=[self.required_token])
         self.standard_input: InstructionInput = InstructionInput(
             tokensets=[self.required_tokenset, self.input_tokenset])
+        self.unique_outputs: set[str] = self._get_unique_outputs()
 
     def to_mtp(self) -> Protocol:
         """Converts the CSV data to MTP format."""
-        ids: list[str] = list(self.line_by_id.keys())
-        self._process_instruction(self.instruction_name, ids)
+        self._process_instruction(self.instruction_name)
         return self.protocol
 
-    def get_required_map(self) -> dict:
-        """Creates a map of the required tokens"""
-        required_map: dict[str, str] = {}
-        for line in self.line_by_id.values():
-            if line.requires_str != "" and not pd.isna(line.requires_str):
-                required_id: str = line.requires_str
-                required_line: CSVLine = self.line_by_id.get(required_id)
-                if not required_line:
-                    raise ValueError(f"Required ID {required_id} not found in CSV data.")
-                required_map[line.output_str] = required_line.output_str
-        return required_map
+    def get_required_output_list(self) -> list[str]:
+        """Creates a list of the required outputs"""
+        required_outputs: set[str] = set()
+        for line in self.ordered_lines:
+            if line.required_str != "" and not pd.isna(line.required_str):
+                required_outputs.add(line.required_str)
+        return list(required_outputs)
+
+    def _get_unique_outputs(self) -> set[str]:
+        """
+        Retrieves unique outputs from the CSV data.
+
+        :return: A set of unique outputs.
+        """
+        unique_outputs: set[str] = set()
+        for line in self.ordered_lines:
+            if line.output_str != "" and not pd.isna(line.output_str):
+                unique_outputs.add(line.output_str)
+        if "GUARDRAIL" in unique_outputs:
+            unique_outputs.remove("GUARDRAIL")
+        return unique_outputs
 
     def _process_dataframe(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         """
@@ -78,46 +87,44 @@ class CSVConversion:
         dataframe.reset_index(drop=True)
         return dataframe
 
-    def _identify_lines(self) -> dict[str, CSVLine]:
+    def _format_lines(self) -> List[CSVLine]:
         """
-        Maps lines in the CSV data by their ID.
+        Formats the DataFrame into a list of CSVLine objects.
 
-        :return: A dictionary mapping line IDs to CSVLine objects.
+        :return: A list of CSVLine objects containing the formatted data.
         """
-        latest_output: str = ""
-        line_by_id: dict[str, CSVLine] = {}
-        id_column: pd.Series = self.csv_data[self.id_col]
-        inputs_column: pd.Series = self.csv_data[self.input_col]
-        outputs_column: pd.Series = self.csv_data[self.output_col]
-        context_column: pd.Series = self.csv_data[self.context_col]
-        requires_column: pd.Series = self.csv_data[self.required_col]
+        ordered_lines: List[CSVLine] = []
+        previous_line: CSVLine | None = None
+        for _, line in self.csv_data.iterrows():
+            formatted_line: CSVLine = self._format_line(previous_line, line)
+            ordered_lines.append(formatted_line)
+            previous_line = formatted_line
+        return ordered_lines
 
-        for idx, line_id in enumerate(id_column):
-            idx += 1  # Adjust index to match CSV line numbering
-            latest_output: str = self._assign_latest(outputs_column, idx, latest_output)
+    def _format_line(self, previous_line: CSVLine | None, line: pd.Series) -> CSVLine:
+        """
+        Formats a single line of the DataFrame into a CSVLine object.
 
-            line_by_id[str(line_id)] = CSVLine(
-                id=str(line_id),
-                input_str=inputs_column[idx],
-                output_str=latest_output,
-                context_str=context_column[idx],
-                requires_str=requires_column[idx]
-            )
+        :param line: A pandas Series representing a single line of the DataFrame.
+        :return: A CSVLine object containing the formatted data.
+        """
+        output_str: str = str(line[self.output_col])
 
-        return line_by_id
+        if output_str == "nan" or output_str == "" or pd.isna(output_str):
+            if previous_line is None:
+                raise ValueError("The first line of the CSV cannot have an empty output.")
+            output_str = str(previous_line.output_str)
+
+        return CSVLine(
+            input_str=str(line[self.input_col]),
+            output_str=output_str,
+            context_str=str(line[self.context_col]),
+            required_str=str(line[self.required_col])
+        )
 
     def _generate_required_token(self) -> Token:
         """Generates the required token for the protocol."""
-        required_outputs: set[str] = set()
-        for line in self.line_by_id.values():
-            if line.requires_str != "" and not pd.isna(line.requires_str):
-                required_ids: list[str] = [i for i in line.requires_str.split(",")]
-                for req_id in required_ids:
-                    required_line: CSVLine = self.line_by_id.get(req_id)
-                    if required_line:
-                        required_outputs.add(required_line.output_str)
-                    else:
-                        raise ValueError(f"Required ID {req_id} not found in CSV data.")
+        required_outputs: list[str] = self.get_required_output_list()
 
         desc: str = f"Here are previous responses that are acceptable: {', '.join(required_outputs)}. If a previous response is given then the new response must match the example."
 
@@ -134,14 +141,13 @@ class CSVConversion:
             return latest
         return value
 
-    def _process_instruction(self, instruction: str, ids: list[str]) -> None:
+    def _process_instruction(self, instruction: str) -> None:
         """
         Processes a single instruction and adds it to the protocol.
 
         :param instruction: The instruction name.
-        :param ids: List of IDs where the instruction occurs.
         """
-        instruction_outputs: set[str] = self._get_unique_outputs(ids)
+        instruction_outputs: set[str] = self._get_unique_outputs()
         acceptable_output_string: str = ", ".join(instruction_outputs)
         instruction_token: Token = Token(self.instruction_name,
                                          desc=f"The responses that are acceptable: {acceptable_output_string}.")
@@ -163,8 +169,7 @@ class CSVConversion:
             name=instruction
         )
 
-        for _id in ids:
-            line: CSVLine = self.line_by_id[_id]
+        for line in self.ordered_lines:
 
             if line.is_guardrail:
                 guardrail.add_sample(line.input_str)
@@ -174,15 +179,12 @@ class CSVConversion:
                 instruction.add_context(line.context_str)
 
             # Add any required outputs
-            previous_response: str = NON_TOKEN.value
+            required_prior_output: str = NON_TOKEN.value
 
-            if line.requires_str != "" and not pd.isna(line.requires_str):
-                required_id: str = line.requires_str
-                required_line: CSVLine = self.line_by_id.get(required_id)
-                if required_line:
-                    previous_response: str = required_line.output_str
+            if line.required_str != "" and not pd.isna(line.required_str):
+                required_prior_output: str = line.required_str
 
-            first_input: str = f"Based on the prompt, choose an answer that best fits. Previous answer was {previous_response}."
+            first_input: str = f"Based on the prompt, choose an answer that best fits. Previous answer was {required_prior_output}."
 
             instruction.add_sample(
                 input_snippets=[first_input, line.input_str],
@@ -196,16 +198,3 @@ class CSVConversion:
             instruction.add_guardrail(guardrail=guardrail, tokenset_index=1)
 
         self.protocol.add_instruction(instruction)
-
-    def _get_unique_outputs(self, ids: list[str]) -> set[str]:
-        """
-        Retrieves unique outputs for a given instruction.
-
-        :param ids: List of IDs where the instruction occurs.
-        :return: A set of unique outputs.
-        """
-        unique_outputs: set[str] = set()
-        for _id in ids:
-            line: CSVLine = self.line_by_id[_id]
-            unique_outputs.add(line.output_str)
-        return unique_outputs
