b29b640841bf7bbc865b737d6eb0bf80f0ddbc22
Nikita Olechko <nikita.olechko@gmail.com>
2026-02-27 12:16:57 -0800

    Expose CSV conversion within MTP

--- model_train_protocol/__init__.py
+++ model_train_protocol/__init__.py
@@ -9,6 +9,7 @@ from .common.tokens import Token, NumToken, NumListToken, FinalToken, Snippet, T
 from .common.instructions.output import InstructionOutput, ExtendedResponse
 from .common.instructions import Instruction, ExtendedInstruction
 from .common.guardrails import Guardrail
+from .csv import conversion
 from .Protocol import Protocol
 from .errors import (
     MTPError,
@@ -51,6 +52,7 @@ __all__ = [
     "InstructionOutput",
     "ExtendedResponse",
     "Guardrail",
+    "conversion",
     "MTPError",
     "MTPValueError",
     "MTPTypeError",
